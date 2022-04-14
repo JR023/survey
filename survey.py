0bb0f0596b582fbd4cbd973081f1ba8e53d09d99
Jordan Romero <yeabuddy2night@gmail.com>
2022-04-14 14:16:28 -0700

    survey

--- survey.py
+++ survey.py
@@ -8,15 +8,16 @@ def index():
 
 @app.route('/result', methods=['POST'])
 def survey_result():
-	session['form_name'] = request.form['name']
-	session['form_location'] = request.form['location']
-	session['form_language']= request.form['language']
-	session['form_comment'] = request.form['comments']
-	return redirect ("/show")
+    session['form_name'] = request.form['name']
+    session['form_location'] = request.form['location']
+    session['form_language']= request.form['language']
+    session['form_comment'] = request.form['comments']
+    return redirect ("/show")
+
 #survey result
 @app.route("/show")
 def show_result():
-	return render_template("result.html", form_name=session['form_name'], form_location=session['form_location'], form_language=session['form_language'],form_comment=session['form_comment'])
-
+	# return render_template("result.html", form_name=session['form_name'], form_location=session['form_location'], form_language=session['form_language'],form_comment=session['form_comment'])
+	return render_template("result.html")
 if __name__ == "__main__":
     app.run(debug=True)
